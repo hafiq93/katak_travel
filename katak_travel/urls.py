5482b00020533b2e2d27df3152a41ef6e8a29b68
Hafiq <mfiqr.work@gmail.com>
2024-12-25 17:48:16 +0800

    update urls

--- katak_travel/urls.py
+++ katak_travel/urls.py
@@ -16,14 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.shortcuts import redirect
+from django.shortcuts import redirect 
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', lambda request: redirect('home', permanent=True)),
-    path("__reload__/", include("django_browser_reload.urls")),
+    path('', lambda request: redirect('home_page', permanent=True)),
+    # path("__reload__/", include("django_browser_reload.urls")),
     # path('admin/', admin.site.urls),
     path('', include('page.urls')),
     path('admin/', include('admin_kt.urls')),
