6d189ae08d2c610e189ed81de9b3cb086a2837f9
Hafiq <mfiqr.work@gmail.com>
2024-12-28 15:46:01 +0800

    update

--- page/urls.py
+++ page/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     # Define your URL patterns here
-    path('home/', views.home, name='home_page'),
+    path('home', views.home, name='home_page'),
     path('about_us/', views.about_us, name='about_us'),
     path('contact_us/', views.contact_us, name='contact_us'),
  
