392455495b2be21ff02699ecab6e46da1d0fb97c
Hafiq <mfiqr.work@gmail.com>
2025-04-14 11:11:45 +0800

    merchant

--- merchant_kt/models.py
+++ merchant_kt/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from user_kt.models import User  
 
 # Create your models here.
 class ListMerchantModule(models.Model):
@@ -6,5 +7,17 @@ class ListMerchantModule(models.Model):
     code = models.CharField(max_length=100, unique=True, null=True)  # E.g., "module_users", "module_reports"
     description = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
+class Company(models.Model):
+    name = models.CharField(max_length=255)  # Name of the company
+    department = models.CharField(max_length=255,blank=True, null=True)  # Name of the company
+    address = models.TextField(blank=True, null=True)  # Company address
+    phone_number = models.CharField(max_length=20, blank=True, null=True)  # Company phone number
+    email = models.EmailField(blank=True, null=True)  # Company email
+    website = models.URLField(blank=True, null=True)  # Company website URL
+    # user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="companies",blank=True, null=True)  # Link to User model
+    
     def __str__(self):
         return self.name
